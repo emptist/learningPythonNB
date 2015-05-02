a7c8a197119c59b201cd921d02d82d16424e5502
J.K <emptist@googlegroups.com>
2015-05-02 15:40:09 +0800

    modified

--- getQuote.py
+++ getQuote.py
@@ -3,21 +3,16 @@ Get quote data from 126.net.
 '''
 import urllib
 
-def getQuote(ids ='1150153'):
-        
+def getQuote(ids, d={}):
     try:
-        def _ntes_quote_callback(dct):
-            global jk_quote_d_res # any better way to return dct? pure return doesn't work
-            jk_quote_d_res = dct
-        
-        url = "http://api.money.126.net/data/feed/{ids}".format(ids=ids)
+        def _ntes_quote_callback(dct):   
+            d['q'] = dct
+            
+        url = "http://api.money.126.net/data/feed/{ids}".format(ids= ids)
         req = urllib.request.Request(url)
         res = urllib.request.urlopen(req)
-        data = res.read()
-        exec(data.decode())
-	d = jk_quote_d_res
-	jk_quote_d_res = None
-        return d
+        exec(res.read().decode()) # should be better way there
+        return d['q']
     
     except Exception as e:
         print(str(e))
